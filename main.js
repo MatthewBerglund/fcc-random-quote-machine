dc1c79fb9175275a4cf9d50d34ad7d2458771160
Matthew Berglund <bergitastrophy@gmail.com>
2021-11-16 16:43:54 +0100

    Add JS functionality to render random quotes

--- main.js
+++ main.js
@@ -1,72 +1,49 @@
 const quotes = [
-  { 
-    work: 'The Iliad',
-    quote: 'Hateful to me as the gates of Hades is that man who hides one thing in his heart and speaks another.'
-  },
-  { 
-    work: 'The Iliad',
-    quote: 'Like the generations of leaves, the lives of mortal men. Now the wind scatters the old leaves across the earth, now the living timber bursts with the new buds and spring comes round again. And so with men: as one generation comes to life, another dies away.'
-  },
-  {
-    work: 'The Iliad',
-    quote: 'All things are in the hand of heaven, and Folly, eldest of Jove\'s daughters, shuts men\'s eyes to their destruction. She walks delicately, not on the solid earth, but hovers over the heads of men to make them stumble or to ensnare them.'
-  },
-  {
-    work: 'The Odyssey',
-    quote: 'Men are so quick to blame the gods: they say that we devise their misery. But they themselves—in their depravity—design grief greater than the griefs that fate assigns.'
-  },
-  {
-    work: 'The Odyssey',
-    quote: 'Even his griefs are a joy long after to one that remembers all that he wrought and endured.'
-  },
-  {
-    work: 'The Odyssey',
-    quote: 'Of the many things hidden from the knowledge of man, nothing is more unintelligible than the human heart.'
-  },
-  { 
-    work: 'Bhagavad Gita',
-    quote: 'You have the right to work, but never to the fruit of work. You should never engage in action for the sake of reward, nor should you long for inaction. Perform work in this world, Arjuna, as a man established within himself—without selfish attachments, and alike in success and defeat.'
-  },
-  { 
-    work: 'Bhagavad Gita',
-    quote: 'For the senses wander, and when one lets the mind follow them, it carries wisdom away like a windblown ship on the waters.'
-  },
-  { 
-    work: 'Bhagavad Gita',
-    quote: 'You came here empty handed, and you will leave empty handed. What is yours today belonged to someone else yesterday, and will belong to someone else tomorrow.'
-  },
-  {
-    work: 'Aeneid',
-    quote: 'Do the gods light this fire in our hearts or does each man\'s mad desire become his god?'
-  },
-  {
-    work: 'Aeneid',
-    quote: 'The gates of hell are open night and day; Smooth the descent, and easy is the way: but to return, and view the cheerful skies, in this the task and mighty labor lies.'
-  },
-  {
-    work: 'Aeneid',
-    quote: 'TEach man has his day, and the time of life is brief for all, and never comes again.'
-  },
-  {
-    work: 'Divine Comedy',
-    quote: 'There is no greater sorrow than to recall happiness in times of misery.'
-  },
-  {
-    work: 'Divine Comedy',
-    quote: 'In the middle of the journey of our life I came to myself within a dark wood where the straight way was lost. Ah, how hard a thing it is to tell what a wild, and rough, and stubborn wood this was, which in my thought renews the fear!'
-  },
-  {
-    work: 'Divine Comedy',
-    quote: 'If the present world go astray, the cause is in you, in you it is to be sought.'
-  },
+  {text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia provident dolorum ex vero pariatur suscipit culpa impedit voluptates quos, expedita, placeat tempora optio atque itaque dolore obcaecati, voluptatibus ab cum.', author: 'Author 1'},
+  {text: 'Quote 2', author: 'Author 2'},
+  {text: 'Quote 3', author: 'Author 3'},
+  {text: 'Quote 4', author: 'Author 4'},
+  {text: 'Quote 5', author: 'Author 5'},
+  {text: 'Quote 6', author: 'Author 6'},
+  {text: 'Quote 7', author: 'Author 7'},
+  {text: 'Quote 8', author: 'Author 8'},
+  {text: 'Quote 9', author: 'Author 9'},
+  {text: 'Quote 10', author: 'Author 10'},
+  {text: 'Quote 11', author: 'Author 11'},
+  {text: 'Quote 12', author: 'Author 12'},
+  {text: 'Quote 13', author: 'Author 13'},
+  {text: 'Quote 14', author: 'Author 14'},
+  {text: 'Quote 15', author: 'Author 15'},
+  {text: 'Quote 16', author: 'Author 16'},
+  {text: 'Quote 17', author: 'Author 17'},
+  {text: 'Quote 18', author: 'Author 18'},
+  {text: 'Quote 19', author: 'Author 19'},
+  {text: 'Quote 20', author: 'Author 20'},
+  {text: 'Quote 21', author: 'Author 21'},
+  {text: 'Quote 22', author: 'Author 22'},
+  {text: 'Quote 23', author: 'Author 23'}
 ];
 
-function getRandomQuote(quotesArray) {
-  var randomQuote;
-  return randomQuote;
+bindEvents();
+displayNewQuote();
+
+function bindEvents() {
+  document.querySelector('.new-quote-button').addEventListener('click', displayNewQuote);
 }
 
-function getQuoteAuthor(quote) {
-  var author;
-  return author;
+function displayNewQuote() {
+  var randomQuote = getRandomQuote(quotes);
+  document.querySelector('.quote-text').innerText = randomQuote['text'];
+  document.querySelector('.quote-author').innerText = randomQuote['author'];
+}
+
+function getRandomIndex(array) {
+  var min = Math.ceil(0);
+  var max = Math.floor(array.length);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomQuote(quotesArray) {
+  var randomIndex = getRandomIndex(quotesArray);
+  return quotesArray[randomIndex];
 }
