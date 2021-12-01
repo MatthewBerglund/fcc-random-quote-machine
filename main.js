68c9d5e29985cafc96dd17bda7b9d4b9dcbadc53
Matthew Berglund <bergitastrophy@gmail.com>
2021-12-01 16:38:47 +0100

    Add XMLHttpRequest to fetch quotes.json; add function init() to initialize app with fetched quote data

--- main.js
+++ main.js
@@ -1,40 +1,56 @@
-const displayArea = document.getElementById('display-area');
+const request = new XMLHttpRequest();
+request.open('GET', 'quotes.json');
+request.responseType = 'json';
 
-bindEvents();
-displayNewQuote();
-
-function bindEvents() {
-  const newQuoteButton = document.getElementById('new-quote-button');
-  newQuoteButton.addEventListener('click', handleNewQuoteClick);
-}
-
-function displayNewQuote() {
-  const quotePara = document.getElementById('quote');
-  const refCite = document.getElementById('ref');
-  const quote = getRandomQuote(quotes);
-
-  quotePara.innerText = `“${quote.text}”`;
-  refCite.innerText = quote.movie;
-  displayArea.classList.toggle('show');
-  updateTwitterIntent(quote);
+request.onload = function() {
+  if (request.status === 200) {
+    const quotes = request.response;
+    init(quotes);
+  } else {
+    console.log(`Network request for quotes.json failed.`);
+  }
 };
 
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
+request.send();
 
-function getRandomQuote(quotes) {
-  const index = getRandomIndex(quotes);
-  return quotes[index];
-}
+function init(quotes) {
+  const displayArea = document.getElementById('display-area');
+  bindEvents();
+  displayNewQuote();
 
-function handleNewQuoteClick() {
-  displayArea.classList.toggle('show');
-  setTimeout(displayNewQuote, 350);
-}
+  function bindEvents() {
+    const newQuoteButton = document.getElementById('new-quote-button');
+    newQuoteButton.addEventListener('click', handleNewQuoteClick);
+  }
 
-function updateTwitterIntent(quote) {
-  const twitterLink = document.getElementById('tweet-quote-link');
-  const intentAddress = `https://twitter.com/intent/tweet?text=“${quote.text}”  — ${quote.movie}`;
-  twitterLink.setAttribute('href', intentAddress);
+  function displayNewQuote() {
+    const quotePara = document.getElementById('quote');
+    const refCite = document.getElementById('ref');
+    const quote = getRandomQuote(quotes);
+
+    quotePara.innerText = `“${quote.text}”`;
+    refCite.innerText = quote.movie;
+    displayArea.classList.toggle('show');
+    updateTwitterIntent(quote);
+  };
+
+  function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
+  function getRandomQuote(quotes) {
+    const index = getRandomIndex(quotes);
+    return quotes[index];
+  }
+
+  function handleNewQuoteClick() {
+    displayArea.classList.toggle('show');
+    setTimeout(displayNewQuote, 350);
+  }
+
+  function updateTwitterIntent(quote) {
+    const twitterLink = document.getElementById('tweet-quote-link');
+    const intentAddress = `https://twitter.com/intent/tweet?text=“${quote.text}”  — ${quote.movie}`;
+    twitterLink.setAttribute('href', intentAddress);
+  }
 }
