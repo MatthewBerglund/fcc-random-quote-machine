dab40b703cc8492b715d030f291fa6a0e7c6dd7b
Matthew Berglund <bergitastrophy@gmail.com>
2021-12-22 14:17:56 +0100

    Replace external API functionality with quotes.json fetch

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
-const displayArea = document.getElementById('display-area');
-
 const newQuoteButton = document.getElementById('new-quote-button');
 newQuoteButton.addEventListener('click', handleNewQuoteClick);
 
-displayNewQuote();
+const displayArea = document.getElementById('display-area');
+let quotes;
+
+fetch('quotes.json')
+  .then(response => {
+    if (!response.ok) {
+      const quoteBox = document.getElementById('quote-box');
+      quoteBox.textContent = `Network request failed with status ${response.status}: ${response.statusText}. Please try again.`;
+    }
+    return response.json();
+  })
+  .then(json => {
+    quotes = json;
+    displayNewQuote();
+  });
 
 function displayNewQuote() {
-  fetch('https://movie-quote-api.herokuapp.com/v1/quote?censored')
-    .then(response => {
-      if (!response.ok) {
-        const quoteBox = document.getElementById('quote-box');
-        quoteBox.textContent = `Network request failed with status ${response.status}: ${response.statusText}. Please try again.`;
-      }
-      return response.json();
-    })
-    .then(quoteJson => {
-      const quotePara = document.getElementById('quote');
-      quotePara.innerText = `“${quoteJson.quote}”`;
+  const quote = getRandomQuote(quotes);
 
-      const refCite = document.getElementById('ref');
-      refCite.innerText = quoteJson.show;
+  const quotePara = document.getElementById('quote');
+  quotePara.innerText = `“${quote.text}”`;
 
-      displayArea.classList.toggle('show');
-      
-      // Allow quote text to fade in before enabling button
-      setTimeout(() => {
-        newQuoteButton.disabled = false;
-      }, 150);
+  const refCite = document.getElementById('ref');
+  refCite.innerText = quote.movie;
 
-      updateTwitterIntent(quoteJson);
-    });
+  const refYear = document.getElementById('ref-year');
+  refYear.innerText = ` (${quote.year})`;
+
+  displayArea.classList.toggle('show');
+
+  // Allow quote text to fade in before enabling button
+  setTimeout(() => {
+    newQuoteButton.disabled = false;
+  }, 150);
+
+  updateTwitterIntent(quote);
 }
 
 function encodeTweetForURI(tweetText) {
@@ -44,14 +51,19 @@ function encodeTweetForURI(tweetText) {
   return encodedTweet;
 }
 
+function getRandomQuote(quotes) {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
+}
+
 function handleNewQuoteClick() {
   newQuoteButton.disabled = true;
   displayArea.classList.toggle('show');
   setTimeout(displayNewQuote, 350);
 }
 
-function updateTwitterIntent(quoteJson) {
-  const tweetText = encodeTweetForURI(`“${quoteJson.quote}”  — ${quoteJson.show}`);
+function updateTwitterIntent(quote) {
+  const tweetText = encodeTweetForURI(`“${quote.text}”  — ${quote.movie} (${quote.year})`);
   const intentURL = `https://twitter.com/intent/tweet?text=${tweetText}`;
   const twitterLink = document.getElementById('tweet-quote-link');
   twitterLink.setAttribute('href', intentURL);
